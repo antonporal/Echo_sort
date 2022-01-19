1d5b6a1ad6b734b3c901824cad62e32a030a26c2
antonporal <81787126+antonporal@users.noreply.github.com>
2022-01-19 10:22:03 -0600

    try again

--- final_sort.py
+++ final_sort.py
@@ -11,24 +11,27 @@ import os
 import pydicom
 import tkinter as tk 
 from tkinter import filedialog
+print("IMPORTED EVERYTHING")
 
 def resource_path(relative):
     if hasattr(sys, "_MEIPASS"):
         return join(sys._MEIPASS, relative)
     return join(relative)
 
+print("CREATED RESOURCE_PATH FUNCITON")
 #GUI for selecting DICOM folder
 window = tk.Tk()
 root = tk.Tk()
 root.withdraw()
-
+print("OPEN TK INSTANCES")
 data_dir = filedialog.askdirectory()
-
+print("ASK FOR FOLDER")
 #Get all the files in the top level of the DICOM folder
 files = [f for f in listdir(data_dir) if isfile(resource_path(join(data_dir, f)))]
-
+print("GET FILES")
 #Change directory to DICOM folder
 os.chdir(data_dir)
+print("CHANGE DIRECTORIES")
 for f in files:
     if f.startswith("IM"):
         #read DICOM
@@ -43,7 +46,9 @@ for f in files:
         else:
             newPath = folder+"\\"+f
             ds.save_as(newPath)
+print("DONE WITH FOR LOOP")
 window.title(data_dir)
 window.after(1000, window.quit)
 window.after(1000, window.destroy)
+print("DONE WITH PROGRAM")
 window.mainloop()
